c985e34b5fbbdc92220ab2c8604b6f7e3d702c51
matti909 <matias.saantiago@gmail.com>
2023-12-13 10:14:53 -0300

    client: comment & like graph ready

--- packages/client/src/app/core/gql.services.ts
+++ packages/client/src/app/core/gql.services.ts
@@ -5,8 +5,74 @@ import * as Apollo from 'apollo-angular';
   @Injectable({
     providedIn: 'root'
   })
+  export class GetCommentsByPostIdGQL extends Apollo.Query<Operations.GetCommentsByPostIdQuery, Operations.GetCommentsByPostIdQueryVariables> {
+    document = Operations.GetCommentsByPostIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class CommentPostGQL extends Apollo.Mutation<Operations.CommentPostMutation, Operations.CommentPostMutationVariables> {
+    document = Operations.CommentPostDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class RemoveCommentGQL extends Apollo.Mutation<Operations.RemoveCommentMutation, Operations.RemoveCommentMutationVariables> {
+    document = Operations.RemoveCommentDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class GetLikesByPostIdGQL extends Apollo.Query<Operations.GetLikesByPostIdQuery, Operations.GetLikesByPostIdQueryVariables> {
+    document = Operations.GetLikesByPostIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class LikePostGQL extends Apollo.Mutation<Operations.LikePostMutation, Operations.LikePostMutationVariables> {
+    document = Operations.LikePostDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class RemoveLikeGQL extends Apollo.Mutation<Operations.RemoveLikeMutation, Operations.RemoveLikeMutationVariables> {
+    document = Operations.RemoveLikeDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
   export class UploadFileGQL extends Apollo.Mutation<Operations.UploadFileMutation, Operations.UploadFileMutationVariables> {
-    override document = Operations.UploadFileDocument;
+    document = Operations.UploadFileDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
@@ -17,7 +83,7 @@ import * as Apollo from 'apollo-angular';
     providedIn: 'root'
   })
   export class CreatePostGQL extends Apollo.Mutation<Operations.CreatePostMutation, Operations.CreatePostMutationVariables> {
-    override document = Operations.CreatePostDocument;
+    document = Operations.CreatePostDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
@@ -28,7 +94,29 @@ import * as Apollo from 'apollo-angular';
     providedIn: 'root'
   })
   export class RemovePostGQL extends Apollo.Mutation<Operations.RemovePostMutation, Operations.RemovePostMutationVariables> {
-    override document = Operations.RemovePostDocument;
+    document = Operations.RemovePostDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class GetPostsByUserIdGQL extends Apollo.Query<Operations.GetPostsByUserIdQuery, Operations.GetPostsByUserIdQueryVariables> {
+    document = Operations.GetPostsByUserIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class GetFeedGQL extends Apollo.Query<Operations.GetFeedQuery, Operations.GetFeedQueryVariables> {
+    document = Operations.GetFeedDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
@@ -39,7 +127,7 @@ import * as Apollo from 'apollo-angular';
     providedIn: 'root'
   })
   export class SetUserPhotoGQL extends Apollo.Mutation<Operations.SetUserPhotoMutation, Operations.SetUserPhotoMutationVariables> {
-    override document = Operations.SetUserPhotoDocument;
+    document = Operations.SetUserPhotoDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
@@ -50,7 +138,7 @@ import * as Apollo from 'apollo-angular';
     providedIn: 'root'
   })
   export class SetUserCoverGQL extends Apollo.Mutation<Operations.SetUserCoverMutation, Operations.SetUserCoverMutationVariables> {
-    override document = Operations.SetUserCoverDocument;
+    document = Operations.SetUserCoverDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
@@ -61,7 +149,7 @@ import * as Apollo from 'apollo-angular';
     providedIn: 'root'
   })
   export class SetUserBioGQL extends Apollo.Mutation<Operations.SetUserBioMutation, Operations.SetUserBioMutationVariables> {
-    override document = Operations.SetUserBioDocument;
+    document = Operations.SetUserBioDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
